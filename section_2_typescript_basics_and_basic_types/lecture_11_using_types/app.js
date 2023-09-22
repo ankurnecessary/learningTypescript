1f3da0f467aa20f00c2624768a4e9c54807c77a1
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-22 12:24:00 +0930

    Lecture 12. TypeScript Types vs JavaScript Types

--- section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.js
+++ section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.js
@@ -1,8 +1,11 @@
 function add(n1, n2) {
+    if (typeof (n1) !== 'number' || typeof (n2) !== 'number') {
+        throw new Error('Invalid inputs!');
+    }
     return n1 + n2;
 }
-// const number1 = '5';
-var number1 = 5;
+var number1 = '5';
+// const number1 = 5;
 var number2 = 2.8;
 var result = add(number1, number2);
 console.log(result);
