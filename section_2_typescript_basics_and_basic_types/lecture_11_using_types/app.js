c261132ed4e97d40f65ae6ea0e080bc2deec36e3
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-25 07:20:02 +0930

    Lecture 20: Working with Enums

--- section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.js
+++ section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.js
@@ -1,8 +1,29 @@
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]
+// } = {
+//     name: 'Ankur'
+//     , age: 40
+//     , hobbies: ['sports', 'cooking']
+//     , role: [2, 'student']
+// }
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "Admin";
+    Role[Role["READ_ONLY"] = 0] = "READ_ONLY";
+    Role["AUTHOR"] = "Author";
+})(Role || (Role = {}));
 var person = {
     name: 'Ankur',
     age: 40,
-    hobbies: ['sports', 'cooking']
+    hobbies: ['sports', 'cooking'],
+    role: Role.ADMIN
 };
+if (person.role === Role.ADMIN) {
+    console.log('Is admin ->' + Role.ADMIN);
+}
 var favouriteActivities;
 favouriteActivities = ['skye'];
 console.log(person);
--- section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.ts
+++ section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.ts
@@ -1,13 +1,26 @@
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string]
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]
+// } = {
+//     name: 'Ankur'
+//     , age: 40
+//     , hobbies: ['sports', 'cooking']
+//     , role: [2, 'student']
+// }
+
+enum Role { ADMIN = 'Admin', READ_ONLY = 0, AUTHOR = 'Author' }
+
+const person = {
     name: 'Ankur'
     , age: 40
     , hobbies: ['sports', 'cooking']
-    , role: [2, 'student']
+    , role: Role.ADMIN
+}
+
+if (person.role === Role.ADMIN) {
+    console.log('Is admin ->' + Role.ADMIN);
 }
 
 let favouriteActivities: string[];
