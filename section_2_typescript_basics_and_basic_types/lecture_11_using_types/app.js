44100b1be3b57383a1afb8c98a5a86d14a5f46b5
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-24 17:34:59 +0930

    Lecture 14. Working with Numbers, Strings & Booleans

--- section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.js
+++ section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.js
@@ -1,11 +1,14 @@
-function add(n1, n2) {
-    if (typeof (n1) !== 'number' || typeof (n2) !== 'number') {
-        throw new Error('Invalid inputs!');
+function add(n1, n2, showResult, phrase) {
+    if (showResult) {
+        console.log("".concat(phrase, " ").concat(n1 + n2));
     }
-    return n1 + n2;
+    else {
+        return "".concat(phrase, " ").concat(n1 + n2);
+    }
 }
-var number1 = '5';
-// const number1 = 5;
+// const number1 = '5';
+var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = 'Result is:';
+var result = add(number1, number2, printResult, resultPhrase);
