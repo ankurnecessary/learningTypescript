030835c8c560211d1e973298f7dd6839f577e96e
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-24 18:15:25 +0930

    Lecture 16: Object Types

--- section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.js
+++ section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.js
@@ -1,14 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    if (showResult) {
-        console.log("".concat(phrase, " ").concat(n1 + n2));
-    }
-    else {
-        return "".concat(phrase, " ").concat(n1 + n2);
-    }
-}
-var number1;
-number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is:';
-var result = add(number1, number2, printResult, resultPhrase);
+var person = {
+    name: 'Ankur',
+    age: 40
+};
+console.log(person);
