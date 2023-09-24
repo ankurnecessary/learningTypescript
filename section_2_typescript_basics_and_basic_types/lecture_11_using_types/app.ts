eec103f667aa2ca7db7a703e791f3fd8fe6fef19
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-25 07:01:12 +0930

    Lecture 19: Working with Tuples

--- section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.ts
+++ section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.ts
@@ -1,7 +1,13 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]
+} = {
     name: 'Ankur'
     , age: 40
     , hobbies: ['sports', 'cooking']
+    , role: [2, 'student']
 }
 
 let favouriteActivities: string[];
