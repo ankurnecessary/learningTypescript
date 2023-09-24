7f25cd53ad6c463d810dadc19a1aac53c3bfa7af
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-24 17:59:14 +0930

    Lecture 15: Type Assignment & Type Inference

--- section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.js
+++ section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.js
@@ -6,8 +6,8 @@ function add(n1, n2, showResult, phrase) {
         return "".concat(phrase, " ").concat(n1 + n2);
     }
 }
-// const number1 = '5';
-var number1 = 5;
+var number1;
+number1 = 5;
 var number2 = 2.8;
 var printResult = true;
 var resultPhrase = 'Result is:';
