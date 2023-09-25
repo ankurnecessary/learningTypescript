3b8905d172ad3058106b905b79ba79470d6b28ca
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-25 10:51:17 +0930

    Lecture 22: Union Types

--- section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.js
+++ section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.js
@@ -1,33 +1,14 @@
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string]
-// } = {
-//     name: 'Ankur'
-//     , age: 40
-//     , hobbies: ['sports', 'cooking']
-//     , role: [2, 'student']
-// }
-var Role;
-(function (Role) {
-    Role["ADMIN"] = "Admin";
-    Role[Role["READ_ONLY"] = 0] = "READ_ONLY";
-    Role["AUTHOR"] = "Author";
-})(Role || (Role = {}));
-var person = {
-    name: 'Ankur',
-    age: 40,
-    hobbies: ['sports', 'cooking'],
-    role: Role.ADMIN
-};
-if (person.role === Role.ADMIN) {
-    console.log('Is admin ->' + Role.ADMIN);
+function combine(input1, input2) {
+    var result;
+    if (typeof (input1) === 'number' && typeof (input2) === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-var favouriteActivities;
-favouriteActivities = ['skye'];
-console.log(person);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine('Ankur', 'Gupta');
+console.log(combinedNames);
--- section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.ts
+++ section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.ts
@@ -1,33 +1,16 @@
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string]
-// } = {
-//     name: 'Ankur'
-//     , age: 40
-//     , hobbies: ['sports', 'cooking']
-//     , role: [2, 'student']
-// }
-
-enum Role { ADMIN = 'Admin', READ_ONLY = 0, AUTHOR = 'Author' }
-
-const person = {
-    name: 'Ankur'
-    , age: 40
-    , hobbies: ['sports', 'cooking']
-    , role: Role.ADMIN
+function combine(input1: number | string, input2: number | string) {
+    let result;
+    if (typeof (input1) === 'number' && typeof (input2) === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
 
-if (person.role === Role.ADMIN) {
-    console.log('Is admin ->' + Role.ADMIN);
-}
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
 
-let favouriteActivities: string[];
-favouriteActivities = ['skye'];
-
-console.log(person);
-
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
-}
+const combinedNames = combine('Ankur', 'Gupta');
+console.log(combinedNames);
