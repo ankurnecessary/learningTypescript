5e2d257bec4723772b964dcf5cde44553433f812
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-27 13:15:39 +0930

    some changes

--- section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.ts
+++ section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.ts
@@ -2,9 +2,11 @@ function add(n1: number, n2: number) {
     return n1 + n2;
 }
 
-function printNum(n1: number) {
+function printResults(n1: number) {
     console.log(n1);
 }
+
+printResults(add(5, 12));
 
 let combineValues: (a: number, b: number) => number;
 combineValues = add;
