f795369bb8bd5087d3e7aa5218c42b21d6d15a6a
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-27 14:15:23 +0930

    Lecture 30: The "never" Type

--- section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.ts
+++ section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.ts
@@ -1,23 +1,6 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
+function getError(code: number, message: string): never {
+    throw {
+        code
+        , message
+    }
 }
-
-function printResults(n1: number) {
-    console.log(n1);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-    const result = n1 + n2;
-    cb(result);
-}
-
-addAndHandle(10, 20, (result) => {
-    console.log(result);
-    return 'test';
-});
-
-
-printResults(add(5, 12));
-
-let combineValues: (a: number, b: number) => number;
-combineValues = add;
