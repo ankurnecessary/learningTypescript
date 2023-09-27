cc683e4a8585b6fac31a77b219389a2f6c4be434
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-27 13:49:00 +0930

    Lecture 28: Function Types & Callbacks(CB)

--- section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.ts
+++ section_2_typescript_basics_and_basic_types/lecture_11_using_types/app.ts
@@ -6,8 +6,18 @@ function printResults(n1: number) {
     console.log(n1);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+}
+
+addAndHandle(10, 20, (result) => {
+    console.log(result);
+    return 'test';
+});
+
+
 printResults(add(5, 12));
 
 let combineValues: (a: number, b: number) => number;
 combineValues = add;
-
