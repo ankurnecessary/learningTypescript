73635b681dce22d239cadc0dc7768494d54bcca7
Ankur Gupta <ankurnecessary@gmail.com>
2023-10-04 08:08:32 +1030

    Lecture 49: "let" and "const"

--- section_4_next_generation_javascript_and_typescript/lecture_49_let_and_const/src/app.ts
+++ section_4_next_generation_javascript_and_typescript/lecture_49_let_and_const/src/app.ts
@@ -1,1 +1,26 @@
-// Code goes here!
+const username = 'Ankur';
+// username = 'Deepika';
+
+let age = 30;
+age = 40;
+
+function add(a: number, b: number) {
+    let result;
+    result = a + b;
+    return result;
+}
+
+// console.log(result);
+
+if (age > 20) {
+    var isOld = true;
+}
+
+// console.log(isOld); // It is showing an error in TS but it will be fine in JS.
+
+
+if (age > 20) {
+    let isOlder = true;
+}
+
+// console.log(isOlder);
