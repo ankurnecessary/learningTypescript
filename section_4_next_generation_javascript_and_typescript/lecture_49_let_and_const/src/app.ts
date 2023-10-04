7c6faf6dae192a9a25accfc31c6cf912ab4f1404
Ankur Gupta <ankurnecessary@gmail.com>
2023-10-05 05:01:12 +1030

    Lecture 50: Arrow Functions

--- section_4_next_generation_javascript_and_typescript/lecture_49_let_and_const/src/app.ts
+++ section_4_next_generation_javascript_and_typescript/lecture_49_let_and_const/src/app.ts
@@ -4,11 +4,11 @@ const username = 'Ankur';
 let age = 30;
 age = 40;
 
-function add(a: number, b: number) {
-    let result;
-    result = a + b;
-    return result;
-}
+// function add(a: number, b: number) {
+//     let result;
+//     result = a + b;
+//     return result;
+// }
 
 // console.log(result);
 
@@ -19,8 +19,19 @@ if (age > 20) {
 // console.log(isOld); // It is showing an error in TS but it will be fine in JS.
 
 
-if (age > 20) {
-    let isOlder = true;
-}
+// if (age > 20) {
+//     let isOlder = true;
+// }
 
 // console.log(isOlder);
+
+const add = (a: number, b: number) => a + b;
+
+// console.log(add(2, 5));
+
+const printOutput: (a: number | string) => void = output => console.log(output)
+
+printOutput(add(5, 2));
+
+const button = document.querySelector('button');
+button?.addEventListener('click', event => console.log(event));
