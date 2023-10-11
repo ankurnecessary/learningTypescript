ff8440b100cfffc75827ca745af8adc394824fa3
Ankur Gupta <ankurnecessary@gmail.com>
2023-10-11 16:17:51 +1030

    Lecture 97: Another Generic Function

--- section_7_generics/src/app.ts
+++ section_7_generics/src/app.ts
@@ -15,3 +15,14 @@ function merge<T extends {}, U>(objA: T, objB: U) {
 const mergedobj = merge({ name: 'Ankur' }, { age: 40 });
 mergedobj.name;
 
+type lengthy = { length: number };
+
+function countAndDescribe<T extends lengthy>(element: T) {
+    let description = element.length;
+    return [element, description];
+}
+
+countAndDescribe([10]);
+countAndDescribe('');
+
+
