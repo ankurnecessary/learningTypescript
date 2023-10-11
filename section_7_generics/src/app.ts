9ad802638b9d99563c1ee60d2d95387d883f28bd
Ankur Gupta <ankurnecessary@gmail.com>
2023-10-11 15:37:29 +1030

    Lecture 95: Creating a Generic Function

--- section_7_generics/src/app.ts
+++ section_7_generics/src/app.ts
@@ -1,10 +1,17 @@
 
-const names: string[] = ['max', 'manuel'];
-const names1: Array<string> = ['max', 'manuel'];
+// const names: string[] = ['max', 'manuel'];
+// const names1: Array<string> = ['max', 'manuel'];
 
-const promise: Promise<number> = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('this is done')
-    }, 2000);
-})
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('this is done')
+//     }, 2000);
+// })
+
+function merge<T, U>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
+
+const mergedobj = merge({ name: 'Ankur' }, { age: 40 });
+mergedobj.name;
 
